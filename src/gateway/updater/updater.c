dadadaa9da946be9b12cafc74480e14d4298d6b5
Maciej Gajdzica <maciek@ucgosu.pl>
2025-02-28 01:35:47 +0100

    commit: updater: add subscribe on update event

--- src/gateway/updater/updater.c
+++ src/gateway/updater/updater.c
@@ -5,6 +5,13 @@
 
 #include <stdlib.h>
 
+#define MAX_SUBSCRIBERS  16
+
+static update_fun_t subscribers[MAX_SUBSCRIBERS];
+static int32_t subs_cnt;
+
+static void notify(void);
+
 void updater_init(void)
 {
     modbus_init();
@@ -17,5 +24,23 @@ void updater_cycle(void)
     for (const struct cdata_record *r = cdata_get_next(&it); r != NULL; r = cdata_get_next(&it))
     {
         modbus_read_hreg(r->slave, r->reg, r->len, r->val);
+    }
+
+    notify();
+}
+
+void updater_subscribe(update_fun_t f)
+{
+    subscribers[subs_cnt++] = f;
+}
+
+static void notify(void)
+{
+    int32_t i;
+
+    for (i = 0; i < subs_cnt; i++)
+    {
+        if (subscribers[i] != NULL)
+            subscribers[i]();
     }
 }
--- src/gateway/updater/updater.h
+++ src/gateway/updater/updater.h
@@ -2,3 +2,7 @@
 
 void updater_init(void);
 void updater_cycle(void);
+
+typedef void (*update_fun_t)(void);
+
+void updater_subscribe(update_fun_t f);
